484a739d1fcba5cb7d23e670b60e6fe25d836379
Richik SC <sc.richik@gmail.com>
2016-05-09 18:08:26 -0500

    new exceptions

--- src/main/java/com/bankofjava/exceptions/AccountNotFoundException.java
+++ src/main/java/com/bankofjava/exceptions/AccountNotFoundException.java
@@ -1,7 +1,17 @@
+/*
+ * Copyright (c) 2016 Richik Sinha Choudhury and Nick Dimitrov
+ */
+
 package com.bankofjava.exceptions;
 
 /**
  * Created by Richik SC on 5/9/2016.
  */
-public class AccountNotFoundException {
+public class AccountNotFoundException extends Exception {
+  public AccountNotFoundException() {
+    super("We were unable to find the account in our system.");
+  }
+  public AccountNotFoundException(String msg) {
+    super("We were unable to find the account in our system. " + msg);
+  }
 }
--- src/main/java/com/bankofjava/exceptions/CustomerIsInvalidException.java
+++ src/main/java/com/bankofjava/exceptions/CustomerIsInvalidException.java
@@ -8,4 +8,42 @@ package com.bankofjava.exceptions;
  * Created by Richik SC on 5/9/2016.
  */
 public class CustomerIsInvalidException extends Exception {
+  private CustomerIsInvalidException.Reason reason;
+
+  public CustomerIsInvalidException(CustomerIsInvalidException.Reason reason) {
+    super();
+    this.reason = reason;
+  }
+
+  public CustomerIsInvalidException(CustomerIsInvalidException.Reason reason, String msg) {
+    super(msg);
+    this.reason = reason;
+  }
+
+  public CustomerIsInvalidException.Reason getReason() {
+    return reason;
+  }
+
+  @Override
+  public String toString() {
+    String reasonMessage = new String();
+    switch (getReason()) {
+      case IS_UNDERAGE:
+        reasonMessage = "Customer is underage.";
+        break;
+      case NOT_ENOUGH_CREDIT:
+        reasonMessage = "Customer's credit score is too low.";
+        break;
+      case OTHER:
+        reasonMessage = "";
+        break;
+    }
+    return reasonMessage + super.toString();
+  }
+
+  public static enum Reason {
+    IS_UNDERAGE, NOT_ENOUGH_CREDIT, OTHER
+  }
+
+
 }
