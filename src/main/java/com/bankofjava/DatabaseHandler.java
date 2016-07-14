696411e6e2bc70f3fea1c4b226102956e65a2cc0
Richik SC <sc.richik@gmail.com>
2016-07-14 12:56:21 -0500

    Database handling is here!

--- src/main/java/com/bankofjava/DatabaseHandler.java
+++ src/main/java/com/bankofjava/DatabaseHandler.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2016 Richik Sinha Choudhury and Nick Dimitrov
+ */
+
 package com.bankofjava;/*
  * Copyright (c) 2016 Richik Sinha Choudhury and Nick Dimitrov
  */
@@ -16,6 +20,7 @@ public class DatabaseHandler {
   public DatabaseHandler(int port) {
     this.port = port;
     this.hostname = "localhost";
+
   }
   
   public DatabaseHandler(String hostname, int port) {
@@ -34,14 +39,59 @@ public class DatabaseHandler {
         this.hostname + ":" +
         this.port + "/";
 
+
     conn = DriverManager.getConnection(
         "jdbc:" + this.dbms + "://" + dbUrl,
         connectionProps
     );
 
-    System.out.println("Connected to " + this.dbms + "database on" + dbUrl);
+    System.out.println("Connected to " + conn.getMetaData().getDatabaseProductName() + " database" +
+        " on " + dbUrl);
     return conn;
 
+  }
+
+  // Copied from JDBC tutorial
+  public static void printSQLException(SQLException ex) {
+
+    for (Throwable e : ex) {
+      if (e instanceof SQLException) {
+        if (ignoreSQLException(
+            ((SQLException)e).
+                getSQLState()) == false) {
+
+          e.printStackTrace(System.err);
+          System.err.println("SQLState: " +
+              ((SQLException)e).getSQLState());
+
+          System.err.println("Error Code: " +
+              ((SQLException)e).getErrorCode());
+
+          System.err.println("Message: " + e.getMessage());
+
+          Throwable t = ex.getCause();
+          while(t != null) {
+            System.out.println("Cause: " + t);
+            t = t.getCause();
+          }
+        }
+      }
+    }
+  }
+
+  // Copied from JDBCTutorial utilites
+  public static boolean ignoreSQLException(String sqlState) {
+    if (sqlState == null) {
+      System.out.println("The SQL state is not defined!");
+      return false;
+    }
+    // X0Y32: Jar file already exists in schema
+    if (sqlState.equalsIgnoreCase("X0Y32"))
+      return true;
+    // 42Y55: Table already exists in schema
+    if (sqlState.equalsIgnoreCase("42Y55"))
+      return true;
+    return false;
   }
 
 }
--- src/test/java/DatabaseHandlerTest.java
+++ src/test/java/DatabaseHandlerTest.java
@@ -1,5 +1,56 @@
+/*
+ * Copyright (c) 2016 Richik Sinha Choudhury and Nick Dimitrov
+ */
+
+import com.bankofjava.DatabaseHandler;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by Richik SC on 7/13/2016.
  */
 public class DatabaseHandlerTest {
+
+  public static void main(String[] args) throws SQLException {
+
+    // Create a new DatabaseHandler to handle the MySQL DB on localhost port 3306
+    DatabaseHandler dbHandler = new DatabaseHandler(3306);
+    Connection conn = null;
+    try {
+      conn = dbHandler.getConnection();
+      conn.setSchema("bankofjava.2016");
+      ResultSet rs = null;
+      try (Statement stmt = conn.createStatement()) {
+        System.out.println("Statement created!");
+        // Get all names that start with 'S'
+        rs = stmt.executeQuery("select * from `bankofjava.2016`.`customers` where name " +
+            "like 'S%'");
+        // % means wildcard - starts with s - any number of any characters after that
+
+        while (rs.next()) {
+          // Loop through result set and print each of them
+          System.out.printf(
+              " %s | %s | %d | %s %n",
+              rs.getString("id"),
+              rs.getString("name"),
+              rs.getInt("age"),
+              rs.getString("gender")
+          );
+        }
+      } catch (SQLException e) {
+        DatabaseHandler.printSQLException(e);
+      } finally {
+        if (rs != null) rs.close();
+      }
+    } catch (SQLException e) {
+      DatabaseHandler.printSQLException(e);
+    } finally {
+      if (conn != null)
+        conn.close();
+    }
+  }
+
 }
